2dd94d365e6044fa6de685e8ab3eeff66ffad883
SAID <saidattoumani50@gmail.com>
2023-10-29 02:06:04 +0300

    modifier droit

--- components/support/ContactTab.jsx
+++ components/support/ContactTab.jsx
@@ -7,61 +7,36 @@ const ContactTab = () => {
       aria-labelledby="contact-tabterm"
     >
       <div className="same__condition__items text__box__none">
-        <h2>Terms & Conditions</h2>
+        <h2>termes et conditions</h2>
         <p>
-          It is important to take care of the patient, to be followed by the
-          doctor, but it is a time of great pain and suffering. The mass but the
-          element requires time but the price of laughter. There is no such
-          thing as an easy time to target the disease. And he expected hunger
-          and poverty. But the bed vestibule mattis ullamcorper wants but the
-          corporal of disease.
+        Les "termes et conditions" d'un site web sont essentiels pour définir les règles et lignes directrices que les utilisateurs doivent suivre pour utiliser le site. Voici un exemple simplifié de "termes et conditions" pour un site web.
+         Ce modèle est générique et doit être adapté aux besoins spécifiques et aux réglementations applicables à votre site web.
         </p>
       </div>
       <div className="same__condition__items text__box__none">
-        <h3>Use Terms for Itechex services</h3>
+        <h3>Termes et Conditions pour Altesphere</h3>
         <p>
-          He wants but the body of the disease needs to decorate the mass. It is
-          a pleasure that God wants to decorate it with a bow. It is easy, but
-          who benefits from the disease? The life of the shooters of the CNN
-          media. Now the chocolate cartoon is on. Life is a wonderful place.
-          Purus in the mass of time nor feugiat nisl the price of that brown.
-          But now he wants to become a member. Drink at the casino or quiver. A
-          mass of mass of ultrices. To decorate the bronze element of Euismod,
-          if anyone is eleifend. In order to monitor the price of the monitor,s
-          there is no power and the monitor has been suspended. It&#39;s
-          convenient, but it&#39;s necessary to have a good taste.
+        En accédant et en utilisant Altesphere, vous acceptez de vous conformer à ces Termes et Conditions.
         </p>
       </div>
       <div className="same__condition__items text__box__none">
-        <h2>Privacy Policy</h2>
+        <h2> Droit Applicable</h2>
         <p>
-          For there is no need for property now. Vulputate football chocolate
-          recipe hairstyles for yeast or yeast. But for the free one, the taste
-          is ugly . But the bed of the urn of two valleys. But tellus at urn
-          condimentum mattis pellentesque id nibh. We have a pillow for
-          tomorrow, but now.
+        Ces Termes et Conditions sont régis par les lois de VotrePays.
+         Tout litige relatif à ces termes sera soumis à la juridiction des tribunaux de VotreVille, VotrePays.
         </p>
       </div>
       <div className="same__condition__items text__box__none">
-        <h4>Cookies and tracking</h4>
+        <h4> Modification des Termes et Condition</h4>
         <p>
-          Let&#39;s do the homework now. The fear of some eleifent mi in no way.
-          Let us live with dignity tomorrow tincidunt lobortis feugiat. There
-          was some flattery too. A wise man and an employee. To decorate the
-          bronze element of euismod, if anyone is eleifend than the adipiscing
-          life. That course put some of my teenagers&#39; fears at bay.
+        Toute modification importante apportée à ces Termes et Conditions sera annoncée sur notre site web, par le biais d'une notification sur la page d'accueil, d'une alerte par e-mail aux utilisateurs enregistrés, ou par tout autre moyen que nous jugerons approprié.
         </p>
       </div>
       <div className="same__condition__items text__box__none">
-        <h2>User responsibilities</h2>
+        <h2>Droit applicable</h2>
         <p>
-          Let us live well, but we need the bow, said Varus. I&#39;m tired of
-          life now but I want to be dignified. The cat needs to be chased by
-          arrows. It is important to pursue the education of the people who live
-          in the disease. Mus mauris vitae ultricies leo whole maleuada.
-          That&#39;s the truck that was used for the sauce. Pain cartoon itself
-          now.
-        </p>
+        Le "droit applicable" est la loi qui sera utilisée pour interpréter et faire respecter les termes et conditions d'un site web, ainsi que pour trancher tout litige qui pourrait survenir entre le site et ses utilisateurs. Voici une section générique sur le "droit applicable" que vous pourriez trouver dans les termes et conditions d'un site internet
+      .  </p>
       </div>
     </div>
   );
